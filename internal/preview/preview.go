d6c65c13ce567a713a8f248e5a0c89dee09f0197
David Alecrim <d.alecrim@xgeeks.io>
2021-03-04 15:10:32 +0000

    feat(git): clone repository present in request

--- internal/preview/preview.go
+++ internal/preview/preview.go
@@ -1,11 +1,18 @@
 package preview
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/go-git/go-git/v5"
 
 	"github.com/gin-gonic/gin"
 )
+
+const GIT_REPO_PATH = "/tmp/gitrepo"
 
 type Preview struct {
 	Namespace  string `json:"namespace" binding:"required"`
@@ -27,17 +34,30 @@ func postPreviewHandler(ctx *gin.Context) {
 	}
 
 	// TODO: STEP_1 clone the git repository
+	removeDirRecursively(GIT_REPO_PATH)
+
+	_, err = git.PlainClone(GIT_REPO_PATH, false, &git.CloneOptions{
+		URL:      newPreview.Repository,
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
 
 	// TODO: STEP_2 pass the path to the Dockerfile on the root of the git cloned project
-	port, err := getPortFromDockerfile("./Dockerfile")
+	port, err := getPortFromDockerfile("/tmp/gitrepo/Dockerfile")
 	if err != nil {
-		panic(err)
+		ctx.Error(err)
+		return
 	}
+	println("port: ", port)
 
 	// TODO: STEP_3 build docker image and push to repository
 
 	// TODO: STEP_4 deploy helm with the image and pass the port for the service from EXPOSE
 
+	ctx.JSON(http.StatusOK, "Deployed at: ${FILL WITH DNS FROM CMY}")
 }
 
 func getPortFromDockerfile(pathToDockerfile string) (string, error) {
@@ -61,4 +81,23 @@ func getPortFromDockerfile(pathToDockerfile string) (string, error) {
 		return "", fmt.Errorf("Error: ", err)
 	}
 	return "", nil
+}
+
+func removeDirRecursively(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
